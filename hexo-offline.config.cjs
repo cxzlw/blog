dcb62fc6f242b8ad4e1a65060b452e4be790ed15
cxzlw <wsc_zds@qq.com>
2023-08-10 21:04:27 +0800

    update

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -3,24 +3,24 @@ module.exports = {
     globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
     globDirectory: "./public",
     swDest: "./public/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/lib\.baomitu\.com\/.*/,
-        handler: "StaleWhileRevalidate"
-      },
-      {
-        urlPattern: /^https:\/\/at\.alicdn\.com\/.*/,
-        handler: "StaleWhileRevalidate"
-      },
-      {
-        urlPattern: /^https:\/\/blog\.cxzlw\.top\/service-worker\.js$/,
-        handler: "NetworkOnly"
-      }, 
-      {
-        urlPattern: /^https:\/\/blog\.cxzlw\.top\/.*/, 
-        handler: "NetworkFirst"
-      }
-    ], 
+    // runtimeCaching: [
+    //   {
+    //     urlPattern: /^https:\/\/lib\.baomitu\.com\/.*/,
+    //     handler: "StaleWhileRevalidate"
+    //   },
+    //   {
+    //     urlPattern: /^https:\/\/at\.alicdn\.com\/.*/,
+    //     handler: "StaleWhileRevalidate"
+    //   },
+    //   {
+    //     urlPattern: /^https:\/\/blog\.cxzlw\.top\/service-worker\.js$/,
+    //     handler: "NetworkOnly"
+    //   }, 
+    //   {
+    //     urlPattern: /^https:\/\/blog\.cxzlw\.top\/.*/, 
+    //     handler: "NetworkFirst"
+    //   }
+    // ], 
     skipWaiting: true, 
     clientsClaim: true
 }
