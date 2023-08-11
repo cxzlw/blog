6cda78b3ce0a64865ec3c39551c60f1e432444bb
cxzlw <wsc_zds@qq.com>
2023-08-11 23:12:06 +0800

    asdasdasd

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,8 +1,7 @@
 // offline config passed to workbox-build.
 module.exports = {
     globPatterns: [
-      "**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}", 
-      "!index.html"
+      "**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}"
     ],
     globDirectory: "./public",
     swDest: "./public/service-worker.js",
