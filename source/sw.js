24caeb1c79eff00a9cb9b754bbf40b3caac0e77e
cxzlw <wsc_zds@qq.com>
2023-08-15 21:56:15 +0800

    updateeeeeee

--- source/sw.js
+++ source/sw.js
@@ -4,18 +4,24 @@ importScripts(
     'https://lib.baomitu.com/workbox-sw/7.0.0/workbox-sw.min.js'
 );
 
-const {StaleWhileRevalidate} = workbox.strategies; 
+const {StaleWhileRevalidate, NetworkFirst} = workbox.strategies; 
 const {registerRoute} = workbox.routing; 
 const {warmStrategyCache} = workbox.recipes;
+const {clientsClaim} = workbox.core;
 
 const staleWhileRevalidate = new StaleWhileRevalidate(); 
+const networkFirst = new NetworkFirst(); 
 
 registerRoute(/^https:\/\/lib\.baomitu\.com\/.*/, staleWhileRevalidate, "GET"); 
 registerRoute(/^https:\/\/at\.alicdn\.com\/.*/, staleWhileRevalidate, "GET"); 
-registerRoute(/^https:\/\/blog\.cxzlw\.top\/.*/, staleWhileRevalidate, "GET"); 
 registerRoute(/^https:\/\/v1\.hitokoto\.cn\/.*/, staleWhileRevalidate, "GET"); 
-registerRoute(/^http:\/\/localhost:8000\/.*/, staleWhileRevalidate, "GET"); 
+registerRoute(/^https:\/\/blog\.cxzlw\.top\/.*/, staleWhileRevalidate, "GET"); 
+registerRoute(/^http:\/\/localhost:8000\/.*\.html/, networkFirst, "GET"); 
+// registerRoute(/^http:\/\/localhost:8000\/.*/, staleWhileRevalidate, "GET"); 
 
 const urls = self.__WB_MANIFEST.map(element => element["url"]);
 
 warmStrategyCache({urls:urls, strategy:staleWhileRevalidate}); 
+
+skipWaiting()
+clientsClaim()
