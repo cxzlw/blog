c0b46ae4241d35194606558022bfd7deeeb16752
cxzlw <wsc_zds@qq.com>
2024-10-17 20:51:53 +0800

    update statistics

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -6,18 +6,27 @@ hexo.extend.filter.register('theme_inject', function(injects) {
     </span>
     `);
     injects.footer.raw('statistics-div', `
-    <div id="statistics-div" class="statistics">
-        <span id="site_pv" style="display: none">
-            总访问量 
-            <span id="site_pv_value"></span>
-            次
+    <div id="statistics-div" class="statistics-div">
+        <span id="page_mv" class="statistics-span" style="display: none">
+                你访问了这个页面
+                <span id="page_mv_value"></span>
+                次
         </span>
-        <span id="site_uv" style="display: none">
-            总访客数 
-            <span id="site_uv_value"></span>
-            人
-        </span>
+        <div class="pvuv-div">
+            <span id="site_pv" class="statistics-span" style="display: none">
+                总访问量
+                <span id="site_pv_value"></span>
+                次
+            </span>
+            
+            <span id="site_uv" class="statistics-span" style="display: none">
+                总访客数
+                <span id="site_uv_value"></span>
+                人
+            </span>
+        </div>
     </div>`); 
+
     injects.bodyEnd.raw('statistics-js', `
     <script async src="/js/statistics.js"></script>
     `); 
